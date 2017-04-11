deb459ba07f5940b344eebb0561d6136ba1d8bf0
CarabadjacDumitru <carabadjacdumitru@gmail.com>
2017-04-11 15:03:57 +0300

    work 2017 04 11
    desc

--- SwingProject/src/MainFrame.java
+++ SwingProject/src/MainFrame.java
@@ -4,27 +4,46 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
-    private JTextArea textArea;
+    /*private JTextArea textArea;*/
+    private Toolbar toolbar;
+    private TextPanel textPanel;
     private JButton btn;
 
     public MainFrame() {
         super("Hellloowwww");
         setLayout(new BorderLayout());
 
-        textArea = new JTextArea();
-        btn = new JButton("Click");
 
-        add(textArea, BorderLayout.CENTER);
+        textPanel = new TextPanel();
+        btn = new JButton("Click");
+        toolbar = new Toolbar();
+
+
+
+        add(toolbar, BorderLayout.NORTH);
+        add(textPanel, BorderLayout.CENTER);
         add(btn, BorderLayout.SOUTH);
+
         btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+                onBtnPressed();
+            }
+        });
+
+        toolbar.setStringListener(new StringListener() {
+            @Override
+            public void textEmitted(String text) {
+                textPanel.appendText(text);
             }
         });
 
         setSize(600, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    public void onBtnPressed(){
+        textPanel.appendText("1111111111111111111111\n");
     }
 }
---
+++ SwingProject/src/Toolbar.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Toolbar extends JPanel implements ActionListener {
+    private JButton helloButton;
+    private JButton goodbyeButton;
+    private TextPanel textPanel;
+    private StringListener stringListener;
+
+    public Toolbar() {
+        helloButton = new JButton("Hello");
+        goodbyeButton = new JButton("Goodbye");
+
+        helloButton.addActionListener(this);
+        goodbyeButton.addActionListener(this);
+
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        add(helloButton);
+        add(goodbyeButton);
+
+    }
+
+    public void setStringListener(StringListener stringListener1) {
+        this.stringListener = stringListener1;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton clicked = (JButton) e.getSource();
+
+            if (clicked == helloButton) {
+            if(stringListener!=null){
+                stringListener.textEmitted("Hello-StringListener");
+            }
+        } else  if (clicked == goodbyeButton) {
+                if(stringListener!=null){
+                    stringListener.textEmitted("Goodbye-StringListener");
+                }
+        }
+    }
+}
